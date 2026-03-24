dc3cb8fcb30643dffb2520e465a575c93985a0d2
JesusSamuelHernandez <je34.41@gmail.com>
2026-03-24 11:49:15 -0600

    se agrego un tema nuevo

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Configuración global del proyecto
 const CONFIG = {
-    MAPBOX_TOKEN: 'pk.eyJ1IjoiamVzdXMyMTUiLCJhIjoiY21tbWY0b2xwMHdiODJvb2xvOWl1ZjJzMCJ9.13FYCZU82BpGkMqxjy5IYA'
+    MAPBOX_TOKEN: 'pk.eyJ1IjoiamVzdXMyMTUiLCJhIjoiY21tdXJvY2dyMDlyNDJwcHp2cjI4NHZjeiJ9.AHalx-VETwwufBcXaKaHpg'
 };
